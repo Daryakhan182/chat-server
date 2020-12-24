092f25c607828f9a21275eced6a000f0e1a72119
Darya khan <daryakhan182@gmail.com>
2020-12-24 02:18:55 -0800

    test

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 var Message = mongoose.model('Message',{
   user : String,
   text : String,
+  //khan
   //category: String,
  // post: String,
 });
